800584e589f137e0ab83a8a2de350fce6667416b
Autumn <772603532@qq.com>
2021-04-10 18:37:14 +0800

    添加button组件

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Q-UI
+
+## 安装
+使用本框架前，请加入以下全局样式
+margin: 0;
+padding: 0;
+box-sizing: border-box;
---
+++ src/button.vue
@@ -0,0 +1,42 @@
+<template>
+  <button class='g-button'>
+    <g-icon v-if="icon" :name="icon" :class="{[`icon-${iconPosition}`]: true}"></g-icon>
+    <slot />
+  </button>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return !(value !== 'left' && value !== 'right')
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+.g-button {
+  font-size: var(--font-size); height: var(--button-height); padding: 0 1em;
+  border-radius: var(--border-radius); border: 1px solid var(--border-color); background: var(--button-bg); 
+  display: inline-flex; justify-content: center; align-items: center; vertical-align: middle;
+  &:hover { border-color: var(--border-color-hover); }
+  &:active { background-color: var(--button-active-color); }
+  &:focus { outline: none; }
+  .icon { margin-right: .3em; }
+  .icon-right { order: 2; margin-right: 0; margin-left: .3em; }
+}
+</style>
